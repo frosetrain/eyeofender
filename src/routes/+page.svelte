c36debbb7422dd9bebf578963f780a6255a43cd1
Frosetrain <80436189+frosetrain@users.noreply.github.com>
2024-10-25 20:42:02 +0800

    Make a nicer ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
     import { getDistance, getRhumbLineBearing } from "geolib";
-    import { onMount } from "svelte";
+
     const stronghold = [1.3466, 103.845677];
+    let started = false;
     let coords = $state({ latitude: 0, longitude: 0 });
     let heading = $state();
+    let bearingReceived = $state(false);
     let bearing = $derived(
         getRhumbLineBearing(
             { latitude: coords.latitude, longitude: coords.longitude },
@@ -21,31 +23,30 @@
         loading = false;
         coords.latitude = location.coords.latitude;
         coords.longitude = location.coords.longitude;
+        bearingReceived = true;
     }
     function geoError(error: GeolocationPositionError) {
         console.log(error);
     }
     function getPosition() {
+        if (!started) {
+            started = true;
+            startCompassListener((compass) => {
+                heading = compass;
+            });
+        }
         loading = true;
         navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
     }
-    onMount(async () => {
-        startCompassListener((compass) => {
-            console.log("hi", compass);
-            heading = compass;
-        });
-        alert("among us");
-    });
 
+    // https://stackoverflow.com/a/75792316
     function startCompassListener(callback: (compass: number) => void) {
         if (!window["DeviceOrientationEvent"]) {
             alert("DeviceOrientation API not available");
             return;
         }
         let absoluteListener = (e: DeviceOrientationEvent) => {
-            alert("absoluteListener");
             if (!e.absolute || e.alpha == null || e.beta == null || e.gamma == null) {
-                alert("what");
                 return;
             }
             let compass = -(e.alpha + (e.beta * e.gamma) / 90);
@@ -54,7 +55,6 @@
             callback(compass);
         };
         let webkitListener = (e) => {
-            alert("webkitListener");
             let compass = e.webkitCompassHeading;
             if (compass != null && !isNaN(compass)) {
                 callback(compass);
@@ -62,14 +62,11 @@
             }
         };
         function addListeners() {
-            alert("real");
             window.addEventListener("deviceorientationabsolute", absoluteListener);
             window.addEventListener("deviceorientation", webkitListener);
         }
         if (typeof DeviceOrientationEvent.requestPermission == "function") {
-            alert(DeviceOrientationEvent.requestPermission());
             DeviceOrientationEvent.requestPermission().then((response) => {
-                alert("ask");
                 if (response == "granted") {
                     addListeners();
                 } else alert("Permission for DeviceMotionEvent not granted");
@@ -78,10 +75,43 @@
     }
 </script>
 
+<div
+    class="aspect-square ring-8 ring-inset ring-emerald-500 overflow-hidden w-screen sm:w-[640px] rounded-full bg-gray-300"
+>
+    <div
+        class="flex aspect-square w-screen sm:w-[640px] flex-col items-center"
+        class:hidden={!bearingReceived}
+        style="transform: rotate({bearing - heading}deg)"
+    >
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            class="size-12 mt-4"
+        >
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"
+            />
+        </svg>
+    </div>
+</div>
+<button
+    class="absolute top-0 aspect-square w-screen sm:w-[640px] flex justify-center items-center"
+    onclick={getPosition}
+    aria-label="Throw eye of ender"
+>
+    <img
+        class:animate-spin={loading}
+        class="h-32 w-32"
+        src="eye_of_ender.png"
+        alt="An eye of ender."
+    />
+</button>
 <p>Coordinates: {coords.latitude} {coords.longitude}</p>
 <p>Heading: {heading}°</p>
 <p>Bearing: {bearing}°</p>
 <p>Distance: {distance} m</p>
-<button onclick={getPosition} aria-label="Throw eye of ender">
-    <img class:animate-spin={loading} src="eye_of_ender.png" alt="An eye of ender." />
-</button>
